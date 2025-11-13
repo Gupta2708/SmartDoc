b4a8d3532b09662f4e92a2e26128cc2d64181b76
Gupta2708 <guptavaibhav2708@gmail.com>
2025-11-13 22:33:10 +0530

    updated api

--- Bk/api/backk.py
+++ Bk/api/backk.py
@@ -104,6 +104,8 @@ origins = [
     "https://licenseee-lovat.vercel.app",  
     "http://localhost:3000",
     "http://localhost:3001",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
     "http://localhost:5500",
     "http://127.0.0.1:5500",
     "http://localhost:8080",
